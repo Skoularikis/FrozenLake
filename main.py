3fc50b8baa892e62d0c5a49123216515b6ebf9e3
Hela <hela64@hotmail.com>
2020-11-19 18:58:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from frozen_lake import FrozenLake
 
+BIG_LAKE_ROWS = 8
+BIG_LAKE_COLS = 8
+
+SMALL_LAKE_ROWS = 4
+BIG_LAKE_ROWS = 4
+
+BIG_LAKE_GOAL = (7,7)
+SMALL_LAKE_GOAL = (3,3)
+
+START = (0, 0)
+
+SMALL_LAKE_ABS = [(1,1),(1,3),(2,3),(3,0)]
+BIG_LAKE_ABS = [(2,3),(3,5),(4,3),(5,1),(5,2),(5,6),(6,1),(6,4),(6,6),(7,3)]
+
+
+
+
 
 def play(env):
     # up, left, bottom, right
